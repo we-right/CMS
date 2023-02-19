fcd074115d556b9cfbacf3b36d5757549c0a931c
hsy <2760703307@qq.com>
2023-02-19 16:31:38 +0800

    data

--- src/api/article-tab/routes/article-tab.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- * article-tab router
- */
-
-const { createCoreRouter } = require('@strapi/strapi').factories;
-
-module.exports = createCoreRouter('api::article-tab.article-tab');
--- src/api/article-tab/services/article-tab.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- * article-tab service
- */
-
-const { createCoreService } = require('@strapi/strapi').factories;
-
-module.exports = createCoreService('api::article-tab.article-tab');
